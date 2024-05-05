1829379df468d9eb24154dd1c6a0219857051986
madhav <madhav032001@gmail.com>
2024-05-05 09:48:17 +0530

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggle-menu');
+const navLinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+
+
+
